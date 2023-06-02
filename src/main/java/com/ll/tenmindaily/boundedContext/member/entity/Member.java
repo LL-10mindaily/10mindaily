71c58d0782afa79ea633408bbfd1fcf582ea4e9b
havegrit <shin_jaean@naver.com>
2023-06-02 13:30:34 +0900

    feat: Member Entity 추가

---
+++ src/main/java/com/ll/tenmindaily/boundedContext/member/entity/Member.java
@@ -0,0 +1,36 @@
+package com.ll.tenmindaily.boundedContext.member.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
+
+@Entity
+@SuperBuilder
+@NoArgsConstructor
+public class Member {
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private Long id;
+    private String userId;
+    private String username;
+    private String password;
+    private String email;
+    private String providerType;
+    private Boolean emailVerified;
+    private String nickname;
+    private String interest1;
+    private String interest2;
+    private String profileImage;
+    @CreatedDate
+    private LocalDateTime createdAt;
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+}
